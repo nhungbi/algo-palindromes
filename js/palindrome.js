87b2458ddf012cbee9c7b306ed467e0220c91bf8
nhungbi <nhungvan16@gmail.com>
2022-05-26 09:13:22 -0500

    fn

--- js/palindrome.js
+++ js/palindrome.js
@@ -1,3 +1,13 @@
 exports.palindrome = function(word) {
+    if (typeof word != 'string') {
+        word = word.toString() }
+
+    let newString = word.toLowerCase()
+    newString = newString.replace(/[^a-z0-9]/g, '') //remove special all chars not a-z and 0-9
+    const reverse = newString.split("").reverse().join("")
+
+    return reverse === newString
 
 };
+
+console.log(exports.palindrome(434))
--- python/palindrome.py
+++ python/palindrome.py
@@ -1,2 +1,17 @@
 def palindrome(word):
-    # Write code here
+    """
+    Given a string or integer, return True if it is a palindrome,
+    (reads the same backward or forward).
+    """
+    if not isinstance(word, str):
+        word = str(word)
+    
+    word = word.lower()
+    new_string = ''.join(filter(str.isalnum, word)) #before join is what is separating each element 
+
+    reverse = new_string[::-1] 
+    return reverse == new_string
+
+# print(palindrome('Noon'))
+# print(palindrome('Sore was I ere I saw Eros.'))
+# print(palindrome('A man, a plan, a canal -- Panama'))
